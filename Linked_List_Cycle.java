1c77a9519fc61d01b5a5b9036e13803cb1482a75
hoBabu <amansahkumar004@gmail.com>
2023-12-14 21:22:07 +0530

    Cycle detetction

---
+++ Linked_List_Cycle.java
@@ -0,0 +1,35 @@
+/*
+  Leetcode --> 141 Linked_List_Cycle detetction
+  used two pointer approach 
+*/
+public class Linked_List_Cycle {
+    /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head ;
+        ListNode slow = head ;
+        while(fast != null && fast.next != null )
+        {
+            fast = fast.next.next ;
+            slow = slow.next;
+             if(fast == slow)
+            {
+                return true ;
+            }
+        }
+        return false;
+        
+    }
+}
+    
+}
