23634de60a992b2d61aa012711f10467a4011133
hoBabu <amansahkumar004@gmail.com>
2024-01-25 16:54:01 +0530

    Right view of a binary Tree

---
+++ RightSideOfBinaryTrre.java
@@ -0,0 +1,50 @@
+/**
+ * https://leetcode.com/problems/binary-tree-right-side-view/ 
+ * leetcode 199 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class RightSideOfBinaryTrre {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null)
+        {
+            return list;
+
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int levelSize= q.size();
+            TreeNode curr = null;
+            for(int i = 0 ; i<levelSize ; i++)
+            {
+                curr = q.remove();
+                if(curr.left != null)
+                {
+                    q.add(curr.left);
+                }
+                if(curr.right != null)
+                {
+                    q.add(curr.right);
+                }
+            }
+            list.add(curr.val);
+
+        }
+        return list;
+        
+    }
+}
