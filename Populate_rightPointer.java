93a6e7653fed0f476baf0a2f34af3416445e3353
hoBabu <amansahkumar004@gmail.com>
2024-01-25 16:33:38 +0530

    Without using extra space

--- Populate_rightPointer.java
+++ Populate_rightPointer.java
@@ -58,3 +58,30 @@ class Populate_rightPointer{
         
     }
 }
+// without using extra space 
+class Solution {
+    public Node connect(Node root) {
+        if(root== null)
+        {
+            return null;
+        }
+        Node leftMost = root ;
+        while( leftMost.left != null)
+        {
+            Node curr = leftMost ;
+            while(curr != null)
+            {
+                curr.left.next = curr.right; 
+                if(curr.next != null)
+                {
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            leftMost = leftMost.left;
+
+        }
+        return root;
+        
+    }
+}
