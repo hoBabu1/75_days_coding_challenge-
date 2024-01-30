5de082cc28c5129e262f5bd914fe69d6153d2f03
hoBabu <amansahkumar004@gmail.com>
2024-01-30 22:58:58 +0530

    Create a binary tree from inorder and oreorder

---
+++ Tree_From_preorder_Inorder.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Tree_From_preorder_Inorder {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0 )
+        {
+            return null;
+        }
+        int r = preorder[0];
+        int index = 0 ; 
+        TreeNode root = new TreeNode(r);
+        for(int i = 0 ; i<inorder.length ; i++)
+        {
+            if(inorder[i] == r)
+            {
+                index = i;
+            }
+        }
+        root.left = buildTree(Arrays.copyOfRange(preorder , 1 ,index+1),Arrays.copyOfRange(inorder , 0 ,index));
+        root.right = buildTree(Arrays.copyOfRange(preorder , index+1 ,preorder.length ) ,Arrays.copyOfRange(inorder , index+1 , inorder.length)  );
+        return root ;
+    }
+}
